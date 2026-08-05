f9407d1f1beeb70e590d41c539fee0c14e3b968a
SimtheCreator <gamegakgak@gmail.com>
2026-08-06 02:02:54 +0700

    Clean up old project files and update index.html meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import { TrackerProvider } from './context/TrackerContext';
 import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TrackerProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </TrackerProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
